f065db2fcea9ae8a6be4fc9bf0f67ef33c8688b8
Gavin <jasondunken@yahoo.com>
2021-03-15 20:22:36 -0400

    implement file upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 
+import * as XLSX from "xlsx";
+
+import { toXML } from 'jstoxml';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +11,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'lims-xls-to-xml';
+
+  statusMessage = "";
+  xlsArray = [];
+  xmlArray: any;
+
+  onFileChange($event: any) {
+    const target: DataTransfer = <DataTransfer>($event.target);
+    if (target.files.length !== 1) {
+      console.log('XLSX can only load one file at a time');
+      return;
+    }
+    const reader: FileReader = new FileReader();
+    reader.onload = (e: any) => {
+      const bstr: string = e.target.result;
+      const wb: XLSX.WorkBook = XLSX.read(bstr, {type: 'binary'});
+      const wsname: string = wb.SheetNames[0];
+      const ws: XLSX.WorkSheet = wb.Sheets[wsname];
+
+      this.xlsArray = XLSX.utils.sheet_to_json(ws, {header: 1});
+      console.log("xls: ", this.xlsArray);
+      this.xmlArray = toXML(this.xlsArray);
+      console.log('xml: ', this.xmlArray);
+    }
+    reader.onerror = (err) => {
+      this.statusMessage = 'Problem loading file';
+      console.log('FileReader.error: ', err);
+      reader.abort();
+    }
+    reader.readAsBinaryString(target.files[0]);
+  }
 }
