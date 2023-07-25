67855b30d7260d908bf9a86a22b1b7bf06dd6e2c
jasondunken <jasondunken@yahoo.com>
2023-07-24 20:20:38 -0400

    cleanup and formatting

--- app.js
+++ app.js
@@ -10,46 +10,46 @@ if (require("electron-squirrel-startup")) return app.quit();
 let appWindow = null;
 
 function createWindow() {
-  appWindow = new BrowserWindow({
-    width: 720,
-    height: 480,
-    minWidth: 720,
-    minHeight: 480,
-    webPreferences: {
-      nodeIntegration: false,
-      contextIsolation: true,
-      enableRemoteModule: false,
-      preload: path.join(__dirname, "preload.js"),
-    },
-    icon: path.join(__dirname, "src/favicon.ico"),
-  });
-  appWindow.loadFile(path.join(__dirname, "src/index.html"));
-  // appWindow.webContents.openDevTools();
-  appWindow.setMenuBarVisibility(false);
+    appWindow = new BrowserWindow({
+        width: 720,
+        height: 480,
+        minWidth: 720,
+        minHeight: 480,
+        webPreferences: {
+            nodeIntegration: false,
+            contextIsolation: true,
+            enableRemoteModule: false,
+            preload: path.join(__dirname, "preload.js"),
+        },
+        icon: path.join(__dirname, "src/favicon.ico"),
+    });
+    appWindow.loadFile(path.join(__dirname, "src/index.html"));
+    // appWindow.webContents.openDevTools();
+    appWindow.setMenuBarVisibility(false);
 }
 
-app.whenReady().then(createWindow); // passing the function as a callback, not the return of the function
+app.whenReady().then(createWindow);
 
 app.on("window-all-closed", function () {
-  if (process.platform !== "darwin") {
-    app.quit();
-  }
+    if (process.platform !== "darwin") {
+        app.quit();
+    }
 });
 
 app.on("activate", function () {
-  if (win === null) {
-    initWindow();
-  }
+    if (win === null) {
+        initWindow();
+    }
 });
 
 ipcMain.on("loadFile", async (event, path) => {
-  fileConverter.loadFile(appWindow, path);
+    fileConverter.loadFile(appWindow, path);
 });
 
 ipcMain.on("translateXLS", async (event, data) => {
-  fileConverter.translateXLS(appWindow, data);
+    fileConverter.translateXLS(appWindow, data);
 });
 
 ipcMain.on("saveFile", async (event, data) => {
-  fileOutput.saveFile(appWindow, data);
+    fileOutput.saveFile(appWindow, data);
 });
